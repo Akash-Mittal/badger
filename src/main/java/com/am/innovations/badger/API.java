2cd0a8fab6b99ecaa4fc713b38a0d863fc3a68d3
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-30 14:04:29 +0530

    Changes:1.) Removed Unused Code.

--- src/main/java/com/am/innovations/badger/API.java
+++ src/main/java/com/am/innovations/badger/API.java
@@ -2,10 +2,7 @@ package com.am.innovations.badger;
 
 public interface API {
 	String BASE_PATH = "/V_1.0/badges";
-	String GET_ALL_REPOS_CLONE_COMMANDS_BY_USER_NAME = "/all/repos/clone/{user}";
-	String GET_ALL_REPOS_BADGES_BY_USER_NAME = "/all/repos/badges/{user}";
-	String GET_ALL_REPOS_GITHUB_BADGES_BY_USERNAME = "/all/repos/badges/github/{user}";
-	String GET_ALL_REPOS_SONAR_BADGES_BY_USERNAME = "/all/repos/badges/sonar/{user}";
+	String GET_ALL_REPOS_BADGES_BY_USER_NAME = "/all/repos/{user}";
 	String URL_PLACE_HOLDER_REPO_NAME = "REPONAME";
 	String URL_PLACE_HOLDER_USER_NAME = "USERNAME";
 }
