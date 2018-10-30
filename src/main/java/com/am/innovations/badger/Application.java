f3a1c8b417ff519f89fa840e4a758120aad5340b
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-30 12:03:28 +0530

    Changes:1.) RestTemplate to Builder.

--- src/main/java/com/am/innovations/badger/Application.java
+++ src/main/java/com/am/innovations/badger/Application.java
@@ -2,6 +2,7 @@ package com.am.innovations.badger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,8 +14,8 @@ public class Application {
 	}
 
 	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
