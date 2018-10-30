2e45536fb6057e638effe97e81d94b9bec6c7c70
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-30 09:44:48 +0530

    Changes:1.) Updated Package Name.

---
+++ src/main/java/com/am/innovations/badger/controller/GitRepoResponses.java
@@ -0,0 +1,14 @@
+package com.am.innovations.badger.controller;
+
+public class GitRepoResponses {
+	private GitRepoResponse gitRepoResponse[];
+
+	public GitRepoResponse[] getGitRepoResponse() {
+		return gitRepoResponse;
+	}
+
+	public void setGitRepoResponse(GitRepoResponse[] gitRepoResponse) {
+		this.gitRepoResponse = gitRepoResponse;
+	}
+
+}
