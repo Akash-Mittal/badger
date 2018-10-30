7fbdfb7287922f35974698cf34f903cd16ac0a0c
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-30 13:39:29 +0530

    Changes:1.) Increemental.

--- src/main/java/com/am/innovations/badger/controller/RepoController.java
+++ src/main/java/com/am/innovations/badger/controller/RepoController.java
@@ -1,20 +1,19 @@
 package com.am.innovations.badger.controller;
 
 import java.net.URISyntaxException;
-import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.am.innovations.badger.API;
-import com.am.innovations.badger.dto.git.GitRepoResponse;
+import com.am.innovations.badger.enums.OPS;
 import com.am.innovations.badger.service.RepoService;
 
 @RestController
@@ -26,26 +25,10 @@ public class RepoController {
 	@Autowired
 	private RepoService repoService;
 
-	@GetMapping(API.GET_ALL_REPOS_CLONE_COMMANDS_BY_USER_NAME)
-	public String getAllRepoCloneCommandsByUserName(@PathVariable(value = "user") String user)
-			throws URISyntaxException {
-		ResponseEntity<GitRepoResponse[]> response = repoService.callGitHub(user);
-		StringBuilder stringBuilder = new StringBuilder();
-		Arrays.stream(response.getBody()).forEach(repo -> {
-			stringBuilder.append("git clone git@github.com:" + user + "/" + repo.getName() + ".git").append("\n");
-		});
-		return stringBuilder.toString();
-	}
-
-	@GetMapping(API.GET_ALL_REPOS_GITHUB_BADGES_BY_USERNAME)
-	public String getAllGithubBadgesByUserName(@NonNull @PathVariable(value = "user") String user)
-			throws URISyntaxException {
-		return repoService.getAllGithubBadgesByUserName(user);
-	}
-
-	@GetMapping(API.GET_ALL_REPOS_SONAR_BADGES_BY_USERNAME)
-	public String getAllSonarBadgesByUserName(@PathVariable(value = "user") String user) throws URISyntaxException {
-		return repoService.getAllSonarBadgesByUserName(user);
+	@GetMapping(API.GET_ALL_REPOS_BADGES_BY_USER_NAME)
+	public String getBadgesByUserName(@NonNull @PathVariable(value = "user") String user,
+			@RequestParam("facility") OPS ops) throws URISyntaxException {
+		return repoService.getAllRepoBadgesByUserName(user, ops);
 	}
 
 }
