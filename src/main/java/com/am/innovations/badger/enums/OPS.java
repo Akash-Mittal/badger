af915733f4371841d25fe9cfbf3ba95de54581f1
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-30 13:39:37 +0530

    Changes:1.) Increemental.

---
+++ src/main/java/com/am/innovations/badger/enums/OPS.java
@@ -0,0 +1,95 @@
+package com.am.innovations.badger.enums;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+import org.springframework.http.ResponseEntity;
+
+import com.am.innovations.badger.API;
+import com.am.innovations.badger.configurations.yml.BadgesConfiguration;
+import com.am.innovations.badger.dto.git.GitRepoResponse;
+
+public enum OPS implements BiFunction<ResponseEntity<GitRepoResponse[]>, BadgesConfiguration, String> {
+
+	GITHUB {
+
+		@Override
+		public String apply(ResponseEntity<GitRepoResponse[]> t, BadgesConfiguration badgesConfiguration) {
+			final StringBuilder stringBuilder = new StringBuilder();
+			Arrays.stream(t.getBody()).forEach(repo -> {
+				stringBuilder.append(replaceUserNameAndRepoName(badgesConfiguration.getGithub().getIssues(), repo))
+						.append(replaceUserNameAndRepoName(badgesConfiguration.getGithub().getForks(), repo))
+						.append(replaceUserNameAndRepoName(badgesConfiguration.getGithub().getLicense(), repo))
+						.append(replaceUserNameAndRepoName(badgesConfiguration.getGithub().getStars(), repo));
+			});
+			return stringBuilder.toString();
+		}
+
+	},
+	SONAR {
+		@Override
+		public String apply(ResponseEntity<GitRepoResponse[]> t, BadgesConfiguration badgesConfiguration) {
+			final StringBuilder stringBuilder = new StringBuilder();
+			Arrays.stream(t.getBody()).forEach(repo -> {
+				stringBuilder.append(replaceUserNameAndRepoName(badgesConfiguration.getSonar().getMeasure(), repo));
+			});
+			return stringBuilder.toString();
+		}
+
+	},
+	TRAVIS {
+		@Override
+		public String apply(ResponseEntity<GitRepoResponse[]> t, BadgesConfiguration badgesConfiguration) {
+			final StringBuilder stringBuilder = new StringBuilder();
+			Arrays.stream(t.getBody()).forEach(repo -> {
+				stringBuilder.append(replaceUserNameAndRepoName(badgesConfiguration.getTravis().getStatus(), repo));
+			});
+			return stringBuilder.toString();
+		}
+
+	},
+	LGTM {
+		@Override
+		public String apply(ResponseEntity<GitRepoResponse[]> t, BadgesConfiguration badgesConfiguration) {
+			final StringBuilder stringBuilder = new StringBuilder();
+			Arrays.stream(t.getBody()).forEach(repo -> {
+				stringBuilder.append(replaceUserNameAndRepoName(badgesConfiguration.getLgtm().getAlerts(), repo))
+						.append(replaceUserNameAndRepoName(badgesConfiguration.getTravis().getStatus(), repo));
+			});
+			return stringBuilder.toString();
+		}
+
+	},
+	CLONE {
+		@Override
+		public String apply(ResponseEntity<GitRepoResponse[]> t, BadgesConfiguration badgesConfiguration) {
+			final StringBuilder stringBuilder = new StringBuilder();
+			Arrays.stream(t.getBody()).forEach(repo -> {
+				stringBuilder.append(replaceUserNameAndRepoName(badgesConfiguration.getGithub().getClone(), repo));
+
+			});
+			return stringBuilder.toString();
+		}
+
+	},
+	ALL {
+		@Override
+		public String apply(ResponseEntity<GitRepoResponse[]> t, BadgesConfiguration u) {
+			throw new RuntimeException("Operation Not Supported.");
+
+		}
+
+	};
+
+	public static String replaceRepoName(String url, GitRepoResponse gitRepoResponse) {
+		return url.replace(API.URL_PLACE_HOLDER_REPO_NAME, gitRepoResponse.getName());
+	}
+
+	public static String replaceUserName(String url, GitRepoResponse gitRepoResponse) {
+		return url.replace(API.URL_PLACE_HOLDER_USER_NAME, gitRepoResponse.getOwner().getLogin());
+	}
+
+	public static String replaceUserNameAndRepoName(String url, GitRepoResponse gitRepoResponse) {
+		return replaceRepoName(replaceUserName(url, gitRepoResponse), gitRepoResponse).concat(System.lineSeparator());
+	}
+}
