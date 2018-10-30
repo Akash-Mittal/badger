e6c7b48e9daa313557501b310565bb1894e1b27e
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-30 13:39:21 +0530

    Changes:1.) Increemental.

--- src/main/java/com/am/innovations/badger/configurations/yml/Lgtm.java
+++ src/main/java/com/am/innovations/badger/configurations/yml/Lgtm.java
@@ -4,23 +4,23 @@ import java.io.Serializable;
 
 public class Lgtm implements Serializable {
 
-	private Object alerts;
-	private Object grade;
+	private String alerts;
+	private String grade;
 	private final static long serialVersionUID = 8530766049397250557L;
 
-	public Object getAlerts() {
+	public String getAlerts() {
 		return alerts;
 	}
 
-	public void setAlerts(Object alerts) {
+	public void setAlerts(String alerts) {
 		this.alerts = alerts;
 	}
 
-	public Object getGrade() {
+	public String getGrade() {
 		return grade;
 	}
 
-	public void setGrade(Object grade) {
+	public void setGrade(String grade) {
 		this.grade = grade;
 	}
 
