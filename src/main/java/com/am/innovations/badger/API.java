425d16f09f2afe43707ff58f45f202826c6f1b83
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-31 11:38:05 +0530

    Changes;1.) Fixed Below. 	Error: Response body Download {   "timestamp": "2018-10-31T06:07:14.119+0000",   "status": 500,   "error": "Internal Server Error",   "message": "Missing URI template variable 'username' for method parameter of type String",   "path": "/V_1.0/badges/all/repos/%7Buser%7D" }

--- src/main/java/com/am/innovations/badger/API.java
+++ src/main/java/com/am/innovations/badger/API.java
@@ -2,7 +2,7 @@ package com.am.innovations.badger;
 
 public interface API {
 	String BASE_PATH = "/V_1.0/badges";
-	String GET_ALL_REPOS_BADGES_BY_USER_NAME = "/all/repos/{user}";
+	String GET_ALL_REPOS_BADGES_BY_USER_NAME = "/all/repos/{username}";
 	String URL_PLACE_HOLDER_REPO_NAME = "REPONAME";
 	String URL_PLACE_HOLDER_USER_NAME = "USERNAME";
 }
