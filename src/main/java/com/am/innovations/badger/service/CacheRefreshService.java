66121a5046e8a68f354939c50e49c81dede4222f
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-11-02 09:38:51 +0530

    Changes:1.) https://github.com/Akash-Mittal/badger/issues

---
+++ src/main/java/com/am/innovations/badger/service/CacheRefreshService.java
@@ -0,0 +1,8 @@
+package com.am.innovations.badger.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CacheRefreshService implements CacheService {
+
+}
---
+++ src/main/java/com/am/innovations/badger/service/CacheService.java
@@ -0,0 +1,5 @@
+package com.am.innovations.badger.service;
+
+public interface CacheService {
+
+}
